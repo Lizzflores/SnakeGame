5b286d09c6502a16eb80311d103a54686f6a8381
ElizabethFlores <lizflores.dev@gmail.com>
2024-02-02 22:34:43 -0600

    Feat: Set random appearance of food

--- script.js
+++ script.js
@@ -3,17 +3,20 @@
 const board = document.getElementById("game-board");
 
 // Define game variables
+const gridSize = 20;
 let snake = [
   {
     x: 10,
     y: 10,
   },
 ];
+let food = generateFood();
 
 //Draw game map, snake, food
 function draw() {
   board.innerHTML = "";
   drawSnake();
+  drawFood();
 }
 
 //Draw snake
@@ -37,4 +40,20 @@ function createGameElement(tag, className) {
 function setPosition(element, position) {
   element.style.gridColumn = position.x;
   element.style.gridRow = position.y;
+}
+
+//Testing draw function
+draw();
+
+//Draw food function
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
 }
